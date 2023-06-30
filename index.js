f07efc9f59e4d6dcd199f25b71f711c955268a33
Thiago Morais <118579263+thiagomorais16@users.noreply.github.com>
2023-06-30 14:55:13 -0300

    created primary rotas

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hola, tudo bem");
+});
+n
+app.listen(4000, () => {
+    console.log("funcionando na porta 4000")
+});
